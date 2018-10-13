e50a6cee622c11f574b0747329201bb31762f15f
Michael Lee <michaellee2245@gmail.com>
2018-10-13 13:53:08 -0600

    swag_controller works

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require ('express-session');
+const checkForSession = require('./middlewares/checkForSession');
+const swag_controller = require('./controllers/swag_controller');
 
 const app = express();
 
@@ -13,5 +15,8 @@ app.use(session({
     resave: false,
     saveUninitialized: true
 }));
+app.use(checkForSession);
+
+app.get('/api/swag', swag_controller.read);
 
 app.listen(SESSION_PORT, () => console.log(`Server connected and running on port ${SESSION_PORT}`))
